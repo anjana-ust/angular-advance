136dfec47c73f73b39df14cec8131f3294e543c9
anjana-ust <anjana.as@ust.com>
2023-06-10 08:42:51 +0530

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  @Output()
+  public sendData=new EventEmitter<string>();
+  ngOnInit(){
+    
+  }
+  onclick(){
+    this.sendData.emit('this is child data');
+  }
+
+}
